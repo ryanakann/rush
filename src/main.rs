4c25cf05522866fd2745fc82342446a6cfe8230c
Ryan Kann <ryanakann@gmail.com>
2024-06-19 22:14:59 -0400

    Barebones main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io::{stdin, stdout, Error, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let ps1 = "> ";
+    let mut input = String::new();
+
+    loop {
+        print!("{}", ps1.to_string());
+        flush();
+        read_input(&mut input);
+    }
+}
+
+fn flush() {
+    match stdout().flush() {
+        Ok(_) => {},
+        Err(e) => {
+            failure(e);
+        },
+    }
+}
+
+fn read_input(input: &mut String) {
+    match stdin().read_line(input) {
+        Ok(_) => {},
+        Err(e) => {
+            failure(e);
+        },
+    }
+}
+
+fn failure(e: Error) {
+    eprintln!("Command failed: {}", e);
 }
